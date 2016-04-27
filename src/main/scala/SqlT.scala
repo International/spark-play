05b94e69c0697f48ab5336aeb61bc4280beb744b
George Opritescu <ssscripting@gmail.com>
2016-04-27 13:27:41 +0200

    added join example

--- src/main/scala/SqlT.scala
+++ src/main/scala/SqlT.scala
@@ -5,7 +5,8 @@ import org.apache.spark.sql._
   * Created by geo on 27.04.16.
   */
 object SqlT {
-  case class Human(name: String, age:Int)
+  case class Human(id: Int, name: String, age:Int)
+  case class Weapon(id:Int, humanId: Int, name: String)
 
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setAppName("Simple Application")
@@ -16,14 +17,26 @@ object SqlT {
 
     val rd = sc.parallelize(
       List(
-        Human("Ion",20),
-        Human("Marie", 21),
-        Human("Ninja", 40)
+        Human(1, "Ion",20),
+        Human(2, "Marie", 21),
+        Human(3, "Ninja", 40)
       )
     )
+    val weaponsDf = sc.parallelize(
+      List(
+        Weapon(1, 1, "pitchfork"),
+        Weapon(1, 2, "wooden rolling pin")
+      )
+    ).toDF()
+
     val humanDf = rd.toDF()
 
+    println("filtering humans older than 21")
     val olderThan40 = humanDf.filter(humanDf("age") > 21)
     olderThan40.show()
+
+    println("showing humans that have weapons")
+    val humansWithWeapons = humanDf.join(weaponsDf, humanDf("id") ===  weaponsDf("humanId"), "inner")
+    humansWithWeapons.show()
   }
 }
